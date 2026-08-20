c279125b112202768519bfee5a23c184708bc0bf
Minna <minhnga1102@gmail.com>
2026-08-20 10:07:59 +0300

    Module 3 Exercises 1

---
+++ helloMe.py
@@ -0,0 +1,63 @@
+print ("Hello world")
+
+
+print ("My name is Minna")
+
+print ("Hello, world!") 
+
+print ('"Hello world"')
+
+print ("Hello")
+print ("world")
+
+# line break \n
+print ("Hello\nworld")
+
+user = input ("Enter your name: ")
+print ("Nice to meet you, "+user+" !")
+
+points = 50; 
+print (user, points)
+
+tuple = (1,2,3)
+print (tuple)
+
+tuple1 = ("nice to meet you, "+user+"!")
+print (tuple1)
+
+tuple2 = (1,2, tuple, tuple1)
+print (tuple2)
+
+person = {
+    'name':"Nga",
+    'age': "34",
+    'city': "helsinki"
+}
+
+print (person['name'], person['age'], person['city'])
+print ([person]) 
+
+first = -9
+second = 124572
+third = 4.988
+fourth = -4 + 2j
+
+print (first)
+print (second)
+print (third)
+print (fourth)
+print (fourth.real)
+print (fourth.imag)
+
+name = "Nga"
+name1 = 'nga'
+number_as_String = "1234553"
+empty_string = ""
+sentence1= "Nga said:'hello'"
+sentence2 = 'Nga said:"hello"'
+print (name)
+print (name1)
+print (number_as_String)
+print (empty_string)
+print (sentence1)
+print (sentence2)
