22355de042bb800f8c9379e7666511b447699a51
Minna <minhnga1102@gmail.com>
2026-08-20 11:04:25 +0300

    Add Module3 Exercises2

---
+++ module3/Exercises2.py
@@ -0,0 +1,16 @@
+import math
+radius = float (input("Enter the radius of the circle: "))
+#radius = input ("Enter the radius of the circle: ")
+
+#turn input into mnumber, otherwise py get it string
+
+#radius = float (radius)
+#print (float(radius) ** 2 * math.pi)
+
+#print (int(radius)*math.pi)2
+
+#area = print (int(radius)*math.pi)
+
+area = radius ** 2 * math.pi
+
+print ("the area of the circle is " + str(area))
